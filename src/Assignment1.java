71e7a9b87fe3d7b00c80fa1fc2c8f4e268fae420
Ninkuk <ninadk03@gmail.com>
2019-08-31 12:48:46 -0700

    Refactored code for submission. Added comments

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Assignment1 {
     public static void main(String[] args) {
         displayWelcomeHeader(); //This method displays the stylized welcome header text
@@ -9,24 +10,26 @@ public class Assignment1 {
     private static void calculateCosts() {
         Scanner scanKeyboard = new Scanner(System.in);
 
-        System.out.println("\nPlease enter haircut type (adult or kids) : ");
+        System.out.println("\nPlease enter haircut type (adult or kid) : ");
         String haircutType;
+        //This while loop will accept inputs until user types either adult or kid
         while (true) {
             if (scanKeyboard.hasNextLine()) {
-                haircutType = scanKeyboard.nextLine().toLowerCase();
-                if (haircutType.equals("adult") || haircutType.equals("kids")) {
+                haircutType = scanKeyboard.nextLine().toLowerCase(); //In case user types input in all uppercase or "Adult" which isn't equal to "adult"
+                if (haircutType.equals("adult") || haircutType.equals("kid")) {
                     break;
                 } else {
-                    System.out.println("Sorry, please input a valid haircut type (adult or kids)");
+                    System.out.println("Sorry, please input a valid haircut type (adult or kid)");
                 }
             } else {
-                System.out.println("Sorry, please input a valid haircut type (adult or kids)");
+                System.out.println("Sorry, please input a valid haircut type (adult or kid)");
             }
         }
 
 
         System.out.println("\nPlease enter the coupon color code (green-1, blue-2, no coupon-0): ");
         int couponColor;
+        //This while loop will accept inputs until user types a coupon number
         while (true) {
             if (scanKeyboard.hasNextInt()) {
                 couponColor = scanKeyboard.nextInt();
@@ -41,35 +44,34 @@ public class Assignment1 {
         }
 
         double adultCost;
-        double kidsCost;
+        double kidCost;
         switch (couponColor) {
             case 1:
                 adultCost = 3.50;
-                kidsCost = 2.50;
+                kidCost = 2.50;
                 break;
             case 2:
                 adultCost = 2.59;
-                kidsCost = 1.50;
+                kidCost = 1.50;
                 break;
             default:
                 adultCost = 15.50;
-                kidsCost = 10.50;
+                kidCost = 10.50;
         }
 
         double totalCost;
         if (haircutType.equals("adult")) {
             totalCost = adultCost;
         } else {
-            totalCost = kidsCost;
+            totalCost = kidCost;
         }
 
         System.out.println("Your Total: $" + totalCost);
 
-        // Converted input to lowercase in case user typed the answer in lowercase
         System.out.println("More Calculations? (YES/NO)");
-        String rerun = scanKeyboard.next().toLowerCase();
+        String rerun = scanKeyboard.next().toLowerCase(); // In case user types input in all lowercase or "Yes" which isn't equal to "YES" or "yes"
         if (rerun.equals("yes")) {
-            calculateCosts();
+            calculateCosts(); //Recursion
         }
     }
 
