e39f570e6e960a538dc5d96a1bdaec968d83f515
Ninad Kulkarni <ninadk03@gmaik.com>
2019-09-04 16:20:04 -0700

    Added comments and handled the number size

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -2,11 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * ALGORITHM
+ *
+ * Take user input
+ * loop through the numbers and divide the digits on even and odd indexes starting from the end
+ * Add all the digits at even number index and double the odd index digits and them to a list
+ * Iterate through the list and add all the digits in it
+ * finally add the even sum and odd sum and check if the last digit is 0
+ */
 public class Assignment2 {
     public static void main(String[] args) {
         validateCardNumber();
     }
 
+    /**
+     * This method gets the card number by calling the getCardNumber method and then verifies it using the algorithm.
+     * Then it prints if the card is valid or not.
+     */
     private static void validateCardNumber() {
         int cardNumber = getCardNumber();
 
@@ -32,9 +45,9 @@ public class Assignment2 {
 
         int finalSum = evenNumsSum + oddNumsSum;
         if (finalSum % 10 == 0) {
-            System.out.println("The card number is valid");
+            System.out.println("The card number is valid!");
         } else {
-            System.out.println("The card number is not valid");
+            System.out.println("The card number is not valid!");
         }
     }
 
@@ -51,7 +64,11 @@ public class Assignment2 {
         while (true) {
             if (scan.hasNextInt()) {
                 cardNumber = scan.nextInt();
-                break;
+                if (Integer.toString(cardNumber).length() == 8) {
+                    break;
+                } else {
+                    System.out.println("Please enter a valid number");
+                }
             } else {
                 System.out.println("Please enter a valid number");
             }
