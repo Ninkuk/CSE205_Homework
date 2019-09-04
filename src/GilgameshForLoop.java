072578fb6042f0ae7dcab450096e8e1755975b39
Ninad Kulkarni <ninadk03@gmaik.com>
2019-09-04 10:11:35 -0700

    Not HW, ignore it.

---
+++ src/GilgameshForLoop.java
@@ -0,0 +1,9 @@
+public class GilgameshForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i < 13; i++) {
+            System.out.println("When he had gone " + i + " double hour,\n" +
+                    "Dense was the darkness, no light was there\n" +
+                    "It would not let him look behind him.");
+        }
+    }
+}
