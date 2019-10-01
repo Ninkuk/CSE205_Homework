bfa075809ea64376bf62b476c5daf6b6e84e1c80
Ninkuk <ninadk03@gmail.com>
2019-09-30 23:16:53 -0700

    Created "SideQuests" package for random projects and added RiemannSums.java
    Signed-off-by: Ninkuk <ninadk03@gmail.com>

--- src/SideQuests/GilgameshForLoop.java
+++ src/SideQuests/GilgameshForLoop.java
@@ -1,3 +1,5 @@
+package SideQuests;
+
 public class GilgameshForLoop {
     public static void main(String[] args) {
         for (int i = 1; i < 13; i++) {
---
+++ src/SideQuests/RiemannSums.java
@@ -0,0 +1,61 @@
+package SideQuests;
+
+public class RiemannSums {
+    public static void main(String[] args) {
+        int n = 4;
+        int b = 9;
+        int a = 1;
+
+        int x = (b - a) / n;
+        double[] points = {1.05, 1.25, 1.75, 2.45, 2.9, 3.3};
+
+        System.out.println("Left: " + leftRule(x, points));
+        System.out.println("Right: " + rightRule(x, points));
+        System.out.println("Trapezoid: " + trapezoidRule(x, points));
+        System.out.println("Simpson: " + simpsonRule(x, points));
+    }
+
+    public static double leftRule(int x, double[] points) {
+        double ySum = 0.0;
+
+        for (int i = 0; i < points.length - 1; i++) {
+            ySum += points[i];
+        }
+
+        return ySum * x;
+    }
+
+    public static double rightRule(int x, double[] points) {
+        double ySum = 0.0;
+
+        for (int i = 1; i < points.length; i++) {
+            ySum += points[i];
+        }
+
+        return ySum * x;
+    }
+
+    public static double trapezoidRule(int x, double[] points) {
+        double ySum = points[0] + points[points.length - 1];
+
+        for (int i = 1; i < points.length - 1; i++) {
+            ySum += (2 * points[i]);
+        }
+
+        return ySum * x / 2;
+    }
+
+    public static double simpsonRule(int x, double[] points) {
+        double ySum = points[0] + points[points.length - 1];
+
+        for (int i = 1; i < points.length - 1; i++) {
+            if (i % 2 == 0) {
+                ySum += (2 * points[i]);
+            } else {
+                ySum += (4 * points[i]);
+            }
+        }
+
+        return ySum * x / 3;
+    }
+}
