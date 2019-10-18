466d016b9cd7953f8cceb94a51f13e18557a2a64
Ninad Kulkarni <ninadk03@gmaik.com>
2019-10-17 18:22:52 -0700

    Added Assignment7

---
+++ src/Assignment7/PhoneCall.java
@@ -0,0 +1,17 @@
+package Assignment7;
+
+public class PhoneCall {
+    String number;
+    int duration;
+    String date;
+    String callerName;
+    double cost;
+
+    public PhoneCall(String number, int duration, String date, String callerName, double cost) {
+        this.number = number;
+        this.duration = duration;
+        this.date = date;
+        this.callerName = callerName;
+        this.cost = cost;
+    }
+}
