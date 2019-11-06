52c7b3d5136161e307d27b3c35fdbef7ffb28e2c
Ninad Kulkarni <ninadk03@gmail.com>
2019-11-06 16:40:10 -0700

    Finished Assignment8 Layout. Count functions needed
    Signed-off-by: Ninad Kulkarni <ninadk03@gmail.com>

--- src/Assignment8/Main.java
+++ src/Assignment8/Main.java
@@ -1,13 +1,17 @@
 package Assignment8;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,6 +22,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        // Layout
         BorderPane borderPane = new BorderPane();
 
         VBox leftPanel = new VBox();
@@ -39,38 +45,125 @@ public class Main extends Application {
         mediumRB.setToggleGroup(radioGroup);
         largeRB.setToggleGroup(radioGroup);
 
-        //TODO: make it font size
-        smallRB.setUserData("Small");
-        mediumRB.setUserData("Medium");
-        largeRB.setUserData("Large");
+        // radiobutton data is font size
+        smallRB.setUserData(10);
+        mediumRB.setUserData(15);
+        largeRB.setUserData(20);
+
+        largeRB.setSelected(true);
+        textArea.setFont(new Font("Arial", 20));
+
+        radioGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                RadioButton selectedOption = (RadioButton) newValue;
+                textArea.setFont(new Font("Arial", Double.parseDouble(selectedOption.getUserData().toString())));
+            }
+        });
+
+        CheckBox[] checkBoxes = new CheckBox[3];
+        String[] checkBoxValues = {"Word Count", "Character Count", "Vowel Count"};
+        String[] displayedValues = {"", "", ""};
+
+//        for (int i = 0; i < checkBoxes.length; i++) {
+//            CheckBox checkBox = new CheckBox(checkBoxValues[i]);
+//            checkBoxes[i] = checkBox;
+//            analysisPanel.getChildren().add(checkBox);
+//
+//            checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+//                @Override
+//                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+//                    if (newValue) {
+//                        displayedValues
+//                    }
+//                    textField.setText(textField.getText() +);
+//                }
+//            });
+//        }
 
         CheckBox wordCountCB = new CheckBox("Word Count");
         CheckBox characterCountCB = new CheckBox("Character Count");
         CheckBox vowelCountCB = new CheckBox("Vowel Count");
 
+        wordCountCB.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    displayedValues[0] = "Word Count: 5";
+                } else {
+                    displayedValues[0] = "";
+                }
+
+                refreshTextField(textField, displayedValues);
+            }
+        });
+
+        characterCountCB.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    displayedValues[1] = "Character Count: 6";
+                } else {
+                    displayedValues[1] = "";
+                }
+
+                refreshTextField(textField, displayedValues);
+            }
+        });
+
+        vowelCountCB.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    displayedValues[2] = "Vowel Count: 6";
+                } else {
+                    displayedValues[2] = "";
+                }
+
+                refreshTextField(textField, displayedValues);
+            }
+        });
+
+
+        // Layout Styles
+        textArea.setPrefHeight(650);
+        textField.setPrefHeight(650);
+
         sizePanel.getChildren().addAll(smallRB, mediumRB, largeRB);
+        sizePanel.setAlignment(Pos.CENTER);
+        sizePanel.setSpacing(10);
         analysisPanel.getChildren().addAll(wordCountCB, characterCountCB, vowelCountCB);
+        analysisPanel.setAlignment(Pos.CENTER);
+        analysisPanel.setSpacing(10);
 
         leftPanel.getChildren().addAll(textArea, sizePanel);
         leftPanel.setPrefWidth(350);
         leftPanel.setPadding(new Insets(10, 5, 10, 10));
+        leftPanel.setSpacing(10);
         rightPanel.getChildren().addAll(textField, analysisPanel);
         rightPanel.setPrefWidth(350);
         rightPanel.setPadding(new Insets(10, 10, 10, 5));
+        rightPanel.setSpacing(10);
 
         borderPane.setLeft(leftPanel);
         borderPane.setRight(rightPanel);
 
-        //Create Scene
-        Scene scene = new Scene(borderPane, 700, 1000);
+        // Creates Scene
+        Scene scene = new Scene(borderPane, 700, 700);
 
-        // Title
+        // Sets Title
         primaryStage.setTitle("Assignment 8");
 
-        //set the scene
+        // Set the scene
         primaryStage.setScene(scene);
 
-        //show the content on window
+        // Show the content on window
         primaryStage.show();
+    }
+
+    private void refreshTextField(TextField textField, String[] displayedValues) {
+        //Cannot create multiline text in TextField thus results showed in single line
+        //Wouldn't Label be a better and more convenient way to display the output?
+        textField.setText(displayedValues[0] + " " + displayedValues[1] + " " + displayedValues[2]);
     }
 }
