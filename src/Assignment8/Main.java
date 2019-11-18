d141eb93dfce63c506e03239f2afae153f3fb3d0
Ninad Kulkarni <ninadk03@gmail.com>
2019-11-17 18:17:37 -0700

    Assignment 8 ready to ship
    Signed-off-by: Ninad Kulkarni <ninadk03@gmail.com>

--- src/Assignment8/Main.java
+++ src/Assignment8/Main.java
@@ -8,7 +8,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
